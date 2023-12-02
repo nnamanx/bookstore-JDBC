c6d0218e908da3f3b17d73173c94e34f1ab7d873
Laman Khudadatzada <lkhudadatzada@gmail.com>
2023-12-02 21:20:00 +0400

    "building gradle"

---
+++ src/main/java/com/nnamanx/bookstorejdbc/BookstoreJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.nnamanx.bookstorejdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreJdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nnamanx/bookstorejdbc/BookstoreJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnamanx.bookstorejdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
