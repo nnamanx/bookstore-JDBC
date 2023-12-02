00f31d07414a89376fe650f7b40c841fad49cabf
Laman Khudadatzada <lkhudadatzada@gmail.com>
2023-12-02 21:48:40 +0400

    "book entity"

--- src/main/java/com/nnamanx/bookstorejdbc/model/entity/Book.java
+++ src/main/java/com/nnamanx/bookstorejdbc/model/entity/Book.java
@@ -1,4 +1,16 @@
 package com.nnamanx.bookstorejdbc.model.entity;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Book {
+
+    Long id;
+    String title;
+    String author;
 }
