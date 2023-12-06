65509a7ab1d30052eb27ecbef5400f0f9c7ed4ec
Laman Khudadatzada <lkhudadatzada@gmail.com>
2023-12-06 18:37:17 +0400

    "Book Service: editing name of the table"

--- java_app/src/service/BookService.java
+++ java_app/src/service/BookService.java
@@ -13,7 +13,7 @@ public class BookService {
 
     public void insertBook(String title, String isbn, int quantity, double price, int authorId) throws SQLException {
 
-        String sql = "INSERT INTO books (title, isbn, quantity_in_stock, price, author_id) VALUES (?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO book (title, isbn, quantity_in_stock, price, author_id) VALUES (?, ?, ?, ?, ?)";
 
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
 
