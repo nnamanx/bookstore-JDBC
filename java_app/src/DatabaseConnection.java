73e7f54a1e8ac278046eae2e4d211e263fffbd4f
Laman Khudadatzada <lkhudadatzada@gmail.com>
2023-12-06 16:47:35 +0400

    "creating connection to database"

---
+++ java_app/src/DatabaseConnection.java
@@ -0,0 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class DatabaseConnection {
+    public Connection databaseConnection(String databaseName, String user, String pass) {
+
+        Connection conn = null;
+
+        try {
+
+            Class.forName("org.postgresql.Driver");
+
+            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + databaseName, user, pass);
+            if (conn != null) {
+
+                System.out.println("Connected.");
+            } else {
+                System.out.println("Failed!");
+            }
+
+        } catch (Exception e) {
+
+            System.out.println(e);
+        }
+        return conn;
+    }
+
+}
